9f41a257d7a2639d36bc2b5cf2b3322bcc560ce4
Brian Du <brian.bh.du@gmail.com>
2018-12-08 10:19:19 -0500

    Add default font and heading styles

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 
 const App = styled.div`
+  font-family: 'Source Sans Pro', Helvetica, sans-serif;
+  font-size: 16px;
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Playfair Display', Georgia, serif;
+    font-weight: 700;
+  }
+
+  h1 {
+    font-size: 4em;
+  }
+  
+  h2 {
+    font-size: 2em;
+  }
+
+  h3 {
+    font-size: 1.5em;
+  }
 `;
 
 export default () => (
   <App>
   </App>
-)
+);
