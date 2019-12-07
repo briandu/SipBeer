3f196ec8b13b72d89bfbaf636095547aec2d8b78
Brian Du <brian.bh.du@gmail.com>
2019-12-07 13:14:17 -0500

    Refactor App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
-
-export default App;
+export default () => (
+  <div className="App">
+  </div>
+)
