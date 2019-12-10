ab2150106872668d3c582e0f05999854626945ab
Brian Du <brian.bh.du@gmail.com>
2019-12-09 23:14:31 -0500

    Create InputField component

---
+++ src/InputField.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { color } from './components/variables';
+
+const InputField = styled.div`
+  margin: 1.5em 0;
+`;
+
+const Label = styled.label`
+  font-size: .8em;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  margin-top: .5em;
+  padding: .75em 1em;
+  font-size: 16px;
+  border: 1px solid ${color.grey30};
+  border-style: solid;
+  border-radius: .2em;
+`;
+
+export default ({ label, type, name }) => (
+  <InputField>
+    <Label>
+      {label}
+      <Input type={type} name={name} />
+    </Label>
+  </InputField>
+);
