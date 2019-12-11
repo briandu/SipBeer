0521ca5f8814568e437dd7bbc73d47b49e494fa4
Brian Du <brian.bh.du@gmail.com>
2019-12-11 00:41:44 -0500

    Update Button component to take in props

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,14 +5,22 @@ import { color } from './variables';
 
 const Button = styled.a`
   display: inline-block;
-  padding: 1.2em 2em;
+  padding: ${props => props.small ? ".8em 1.2em" : "1.2em 2em"};
   color: ${color.white};
   background-color: ${color.brand};
   border-radius: 10em;
+  cursor: pointer;
+  transition: .2s ease;
+
+  :hover {
+    background-color: ${color.brandDark};
+  }
 `;
 
-export default ({ text, onClick }) => (
-  <Button onClick={onClick}>
+export default ({
+  className, text, onClick, small,
+}) => (
+  <Button className={className} onClick={onClick} small={small}>
     {text}
   </Button>
 );
