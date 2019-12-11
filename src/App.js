04e1e26dd6f038ac2cb27fdf36f4fb13bd2acf09
Brian Du <brian.bh.du@gmail.com>
2019-12-11 17:22:17 -0500

    move global styles to GlobalStyles.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-import { color, breakpoint } from './components/variables';
+import GlobalStyle from './components/GlobalStyles';
 import NavHeader from './components/NavHeader';
 import NotficationBar from './components/NotficationBar';
 import Hero from './components/Hero';
@@ -10,65 +10,15 @@ import PricingPlans from './components/PricingPlans';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
-const App = styled.div`
-  font-size: 16px;
-  font-family: 'Source Sans Pro', Helvetica, sans-serif;
-
-  h1, h2, h3, h4, h5, h6 {
-    font-weight: 700;
-    font-family: 'Playfair Display', Georgia, serif;
-  }
-
-  h1 {
-    font-size: 3em;
-  }
-  h2 {
-    font-size: 1.8em;
-  }
-  h3 {
-    font-size: 1.5em;
-  }
-  h4 {
-    font-size: 1.2em;
-  }
-  h5 {
-    font-size: 1em;
-  }
-
-  p {
-    color: ${color.grey70};
-    font-size: .875em;
-    line-height: 1.5em;
-  }
-
-  @media ${breakpoint.large} { /* tablet size 768px */
-    h1 {
-      font-size: 4em;
-    }
-    h2 {
-      font-size: 2em;
-    }
-    h3 {
-      font-size: 1.5em;
-    }
-    h4 {
-      font-size: 1.25em;
-    }
-
-    p {
-      font-size: 1em;
-    }
-  }
-`;
-
-const Main = styled.main`
-`;
+const App = styled.div``;
+const Main = styled.main``;
 
 export default () => {
   const [noficationIsActive, setNotificationIsActive] = useState(false);
 
   return (
     <App>
+      <GlobalStyle />
       <NotficationBar
         noficationIsActive={noficationIsActive}
       />
---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from 'styled-components';
+
+import { color, breakpoint } from './variables';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-size: 16px;
+    font-family: 'Source Sans Pro', Helvetica, sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 700;
+    font-family: 'Playfair Display', Georgia, serif;
+  }
+
+  h1 {
+    font-size: 3em;
+  }
+  h2 {
+    font-size: 1.8em;
+  }
+  h3 {
+    font-size: 1.5em;
+  }
+  h4 {
+    font-size: 1.2em;
+  }
+  h5 {
+    font-size: 1em;
+  }
+
+  p {
+    color: ${color.grey70};
+    font-size: .875em;
+    line-height: 1.5em;
+  }
+
+  @media ${breakpoint.large} { /* tablet size 768px */
+    h1 {
+      font-size: 4em;
+    }
+    h2 {
+      font-size: 2em;
+    }
+    h3 {
+      font-size: 1.5em;
+    }
+    h4 {
+      font-size: 1.25em;
+    }
+
+    p {
+      font-size: 1em;
+    }
+  }
+`;
+
+export default GlobalStyle;
