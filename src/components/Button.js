584dc2abdf351b6e1e7a96c4b093147f406ba202
Brian Du <brian.bh.du@gmail.com>
2019-12-10 23:46:53 -0500

    Create button component

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { color } from './variables';
+
+const Button = styled.a`
+  display: inline-block;
+  padding: 1.2em 2em;
+  color: ${color.white};
+  background-color: ${color.brand};
+  border-radius: 10em;
+`;
+
+export default ({ text }) => (
+  <Button>
+    {text}
+  </Button>
+);
