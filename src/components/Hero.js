d748ae92e8811386c80bd055364a5b1023fc7a97
Brian Du <brian.bh.du@gmail.com>
2019-12-10 23:46:31 -0500

    Add Hero image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 import { color } from './variables/index';
+import heroImg from '../assets/images/hero.png';
+import Button from './Button';
 
 const Hero = styled.section`
-  height: 80vh;
-  padding-top: 7em;
+  min-height: 38em;
+  margin-top: 7em;
   text-align: center;
+`;
+
+const Copy = styled.div`
+  
 `;
 
 const Title = styled.h1`
 `;
 
 const Description = styled.p`
-  margin-top: 2em;
+  margin: 2em 0 3em;
+`;
+
+const SignUpButton = styled(Button)`
+  margin-top: 3em;
+`;
+
+const Image = styled.img`
+  max-width: 60em;
+  margin-top: 3em;
+`;
+
+const ScrollGuide = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 4em;
+  height: 4em;
+  margin: 10em auto 0;
+  color: ${color.white};
+  background-color: ${color.brand};
+  border-radius: 10em;
 `;
 
 const AccentText = styled.span`
@@ -25,11 +54,19 @@ const AccentText = styled.span`
 
 export default () => (
   <Hero>
-    <Title>Delicious Craft Beer</Title>
-    <Description>
-      Delivered to your doorsteps
-      <AccentText>monthly</AccentText>
-      .
-    </Description>
+    <Copy>
+      <Title>Delicious Craft Beer</Title>
+      <Description>
+        Delivered to your doorsteps
+        <AccentText>monthly</AccentText>
+        .
+      </Description>
+      <SignUpButton text="Sign up now" />
+    </Copy>
+    <Image src={heroImg} />
+
+    <ScrollGuide>
+     <FontAwesomeIcon icon={faChevronDown} />
+    </ScrollGuide>
   </Hero>
 );
